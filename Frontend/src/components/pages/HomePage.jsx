161abaf789fecdcd5a10901b59c0d7c00128ae99
ester051 <estersula@unyt.edu.al>
2026-01-17 18:55:36 +0100

    Small update

--- Frontend/src/components/pages/HomePage.jsx
+++ Frontend/src/components/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+//This page contains the code for the HomePage of the website.
 import React, { useState, useEffect } from 'react';
 import { Star, Truck, Shield, ChevronLeft, ChevronRight, ShoppingBag, Sparkles } from 'lucide-react';
 import { apiService } from "../services/api";
